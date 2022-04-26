0028fd3c57f368fbb8662b90424bf9f599045214
unknown <bassel_sabour@hotmail.com>
2022-04-26 10:59:36 +0200

    Added CSV Reader Function

--- Book.py
+++ Book.py
@@ -9,17 +9,22 @@ class Book:
         self.author = author
         self.quotes = quotes
 
-    def get_writer_object():
-        csv_file = open("Books.csv", "a")
-        writer_object = csv.writer(csv_file, lineterminator="\n")
-        return writer_object
+    def get_csv_writer():
+        csv_file = open('Books.csv', 'a')
+        csv_writer = csv.writer(csv_file, lineterminator='\n')
+        return csv_writer
+
+    def get_csv_reader():
+        csv_file = open('Books.csv', 'r')
+        csv_reader = csv.reader(csv_file)
+        return csv_reader
 
     def initialize_csv():
-        writer_object = Book.get_writer_object()
+        writer_object = Book.get_csv_writer()
         writer_object.writerow(["Title", "Author", "Quote"])  # header
 
     def write_quotes_in_csv(self):
-        writer_object = Book.get_writer_object()
+        writer_object = Book.get_csv_writer()
         if self.quotes is not None:
             for quote in self.quotes:
                 writer_object.writerow([self.title, self.author, quote])
@@ -50,7 +55,7 @@ class Book:
         Book.fill_csv(Book.get_books())
 
     def set_books_from_csv():
-        pass
+        Book.get_csv_reader()
 
 
 Book.set_books_from_notion()
