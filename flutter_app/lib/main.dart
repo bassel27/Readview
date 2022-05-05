06e3652dfb60acac50f9079fa4bc1abb8e8e90ba
unknown <bassel_sabour@hotmail.com>
2022-05-05 19:02:55 +0200

    Major Code Improvements

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'book.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -7,63 +8,73 @@ void main() {
 }
 
 class Home extends StatelessWidget {
-  //the _ before the class name means that it can't be used outside of this file
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 50, 56, 83),
-      drawer: Drawer(
-          child: Container(
-        color: Color.fromARGB(81, 14, 37, 244),
-      )),
-      appBar: AppBar(
-        toolbarHeight: 100,
-        title: Image.asset('assets/Readview-logos_white.png'),
-        centerTitle: true,
-        backgroundColor: Color.fromARGB(255, 68, 75, 105),
-        elevation: 0.0, // to prevent the dropshadow effect
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start, //vertical in a column
-        crossAxisAlignment: CrossAxisAlignment.stretch, //horizontal in a column
-        children: getHomeButtonsList(),
-      ),
+        backgroundColor: Color.fromARGB(255, 50, 56, 83),
+        drawer: Drawer(
+            child: Container(
+          color: Color.fromARGB(81, 14, 37, 244),
+        )),
+        appBar: AppBar(
+          toolbarHeight: 100,
+          title: Image.asset('assets/Readview-logos_white.png'),
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(255, 68, 75, 105),
+          elevation: 0.0, // to prevent the dropshadow effect
+        ),
+        body: HomeBody());
+  }
+}
+
+class HomeButton extends StatelessWidget {
+  static const sizeFont = 23.0;
+  static const sizeIcon = 30.0;
+  final text;
+  final function;
+  final r;
+  final g;
+  final icon;
+  HomeButton(this.text, this.function, this.r, this.g, this.icon);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: function,
+      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Icon(icon, size: sizeIcon),
+        SizedBox(width: 10),
+        Text(text, style: TextStyle(fontSize: sizeFont)),
+      ]),
+      style: ElevatedButton.styleFrom(
+          minimumSize: Size(200, 80), primary: Color.fromARGB(255, r, g, 208)),
     );
   }
 }
 
-Widget getButton(text, function, r, g, icon) {
-  const sizeFont = 23.0;
-  const sizeIcon = 30.0;
-  return ElevatedButton(
-    onPressed: function,
-    child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Icon(icon, size: sizeIcon),
-      SizedBox(width: 10),
-      Text(text, style: TextStyle(fontSize: sizeFont)),
-    ]),
-    style: ElevatedButton.styleFrom(
-        minimumSize: Size(200, 80), primary: Color.fromARGB(255, r, g, 208)),
-  );
-}
-
-List<Widget> getHomeButtonsList() {
-  int r = 150;
-  int g = 180;
-  const subtractAmount = 20;
+//ignore: must_be_immutable
+class HomeBody extends StatelessWidget {
+  var r = 150;
+  var g = 180;
+  static const subtractAmount = 20;
   void updateQuotes() {}
   void dailyReview() {}
   void favorites() {}
   void browseByTheme() {}
-  return [
-    SizedBox(height: 20),
-    getButton("Daily Review", dailyReview, r, g, Icons.calendar_month),
-    getButton("Update Quotes", updateQuotes, r -= subtractAmount,
-        g -= subtractAmount, Icons.update),
-    getButton("Favorites", favorites, r -= subtractAmount, g -= subtractAmount,
-        Icons.favorite),
-    getButton("Browse by Theme", browseByTheme, r -= subtractAmount,
-        g -= subtractAmount, Icons.theater_comedy),
-  ];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.start, //vertical in a column
+        crossAxisAlignment: CrossAxisAlignment.stretch, //horizontal in a column
+        children: [
+          SizedBox(height: 20),
+          HomeButton("Daily Review", dailyReview, r, g, Icons.calendar_month),
+          HomeButton("Update Quotes", updateQuotes, r -= subtractAmount,
+              g -= subtractAmount, Icons.update),
+          HomeButton("Favorites", favorites, r -= subtractAmount,
+              g -= subtractAmount, Icons.favorite),
+          HomeButton("Browse by Theme", browseByTheme, r -= subtractAmount,
+              g -= subtractAmount, Icons.theater_comedy),
+        ]);
+  }
 }
