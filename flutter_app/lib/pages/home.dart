4e4860732e20b6c8e4dabb7a2fd6dfd83a8c5dcd
unknown <bassel_sabour@hotmail.com>
2022-05-08 09:35:47 +0200

    Function File Transfer

--- flutter_app/lib/pages/home.dart
+++ flutter_app/lib/pages/home.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 //import 'package:readview_app/pages/loading_screen.dart';
 import '/services/daily_review.dart';
-import 'dart:convert';
-import '/classes/book.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -47,7 +45,11 @@ class HomeButton extends StatelessWidget {
         Text(text, style: TextStyle(fontSize: sizeFont)),
       ]),
       style: ElevatedButton.styleFrom(
-          minimumSize: Size(200, 80), primary: Color.fromARGB(255, r, g, 208)),
+        minimumSize: Size(200, 80),
+        primary: Color.fromARGB(255, r, g, 208),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0.0)),
+        elevation: 0,
+      ),
     );
   }
 }
@@ -57,37 +59,6 @@ class HomeBody extends StatelessWidget {
   var r = 150;
   var g = 180;
   static const subtractAmount = 20;
-
-  void dailyReview(context, route) async {
-    void inistantiateClass() {}
-    final tempDict;
-    Navigator.pushNamed(context, route);
-    tempDict = jsonDecode(
-        await fetchData()); // use await to make it know when the async called func is complete (even if this func has void return, you must return Future<void>)
-
-    List<Book> books = [];
-    List<String> quotes = []; // try with empty list?
-    for (var c1 = 0, c2 = 1;
-        c1 < tempDict.length && c2 < tempDict.length;
-        c1++, c2++) {
-      Map bookDict1 = tempDict[c1.toString()]; //without .tostring?
-      Map bookDict2 = tempDict[c2.toString()];
-      quotes.add(bookDict1['Quote']);
-      if (bookDict1['Title'] != bookDict2['Title']) {
-        books.add(Book(bookDict1['Title'], bookDict1['Author'], quotes));
-        quotes = [];
-      }
-      if (c2 + 1 == tempDict.length) {
-        quotes.add(bookDict2['Quote']);
-        books.add(Book(bookDict2['Title'], bookDict2['Author'], quotes));
-        quotes = [];
-      }
-    }
-    for (int i = 0; i < books.length; i++) {
-      print("${books[i].title}, ${books[i].author}, ${books[i].quotes}");
-    }
-    // print(dic);
-  }
 
   void updateQuotes() {}
   void favorites() {}
@@ -100,7 +71,6 @@ class HomeBody extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start, //vertical in a column
         crossAxisAlignment: CrossAxisAlignment.stretch, //horizontal in a column
         children: [
-          SizedBox(height: 20),
           HomeButton("Daily Review", dailyReview, '/dailyReview', r, g,
               Icons.calendar_month),
           HomeButton("Update Quotes", loadingScreen, '', r -= subtractAmount,
