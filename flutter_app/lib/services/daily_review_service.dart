468e817ce8bd7c82ffa312337654bbeca502baae
unknown <bassel_sabour@hotmail.com>
2022-07-12 12:22:34 +0200

    resolved warnings

--- flutter_app/lib/services/daily_review_service.dart
+++ flutter_app/lib/services/daily_review_service.dart
@@ -1,9 +1,1 @@
-import 'package:http/http.dart'
-    as http; // to clarify that get method is used from http package because it look like it's a user-defined function
-
-    
-Future<String> fetchData({String url = "http://10.0.2.2:5000/?query=1"}) async {
-  http.Response response = await http.get(Uri.parse(url));
-  return response
-      .body; // retruns JSON string that has to be decoded to be converted
-}
+// TODO: api part here
