35a815fd716de07e86511220722c22b66c863859
unknown <bassel_sabour@hotmail.com>
2022-07-13 13:04:46 +0200

    Working

--- flutter_app/lib/screens/browse_books_screen.dart
+++ flutter_app/lib/screens/browse_books_screen.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
-class BrowseBooksScreen extends StatelessWidget {
+class BrowseBooksScreen extends StatefulWidget {
   final List<Card> booksCards;
   static const route = '/browseByBook';
   BrowseBooksScreen(this.booksCards);
+
+  @override
+  State<BrowseBooksScreen> createState() => _BrowseBooksScreenState();
+}
+
+class _BrowseBooksScreenState extends State<BrowseBooksScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: this.booksCards,
+      body: ListView.builder(
+        // list view builder is dynamic (laods only what's visible)     //TODO: must be a stateful widget? //it rebuilds when the list updates
+        itemBuilder: ((context, index) {
+          return widget.booksCards[index];
+        }),
+        itemCount: widget.booksCards.length,
       ),
     );
   }
